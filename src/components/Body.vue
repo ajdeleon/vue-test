a78fbbe6282377bf7974a00ad72494082f3b1516
AJ de Leon <ajdeleon1064@gmail.com>
2018-03-29 23:55:44 -0400

    first commit

---
+++ src/components/Body.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="body">
+    Hey this is the body
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'body',
+  data () {
+    return {
+      text: 'Hey this is the body'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #35495E;
+}
+</style>
